11806edfb9faa4055ca4c538b8fc6d5d79baa43e
keerthana <keerthanaselvam05@gmail.com>
2025-03-17 22:14:30 +0530

    Initialize POST

--- internal/staff/model.go
+++ internal/staff/model.go
@@ -1,10 +1,42 @@
 package staff
 
 type Staff struct {
+	ID             string
+	BranchID       string
+	OrganizationID string
+	FirstName      string
+	LastName       string
+	Code           string
+	StaffType      string
+	Status         string
+	Speciality     []string
+	Departments    Departments
+	Schedule       Schedule
+	Email          string
+	Phone          string
+	DateOfBirth    string
+	Gender         string
+	Address        Address
+	Metadata       string
+	CreatedAt      string
+	UpdatedAt      string
 }
 
-type ContactInfo struct {
+type Departments struct {
+	DepartmentID string
+	Role         string
+	IsPrimary    bool
 }
 
-type Pagination struct {
+type Schedule struct {
+	Type   string
+	Shifts []string
+}
+
+type Address struct {
+	Street  string
+	City    string
+	State   string
+	Country string
+	ZipCode string
 }
--- internal/staff/repository.go
+++ internal/staff/repository.go
@@ -1,6 +1,8 @@
 package staff
 
 import (
+	"context"
+
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/null-bd/logger"
 )
@@ -9,6 +11,7 @@ import (
 
 type (
 	IStaffRepository interface {
+		Create(ctx context.Context, dept *Staff) (*Staff, error)
 	}
 
 	staffRepository struct {
@@ -25,3 +28,23 @@ func NewStaffRepository(db *pgxpool.Pool, logger logger.Logger) IStaffRepository
 }
 
 // region SQL Queries
+const (
+	createStaffQuery = `
+	    INSERT INTO staffs (
+			id, branch_id, organization_id, name, code, type, specialty, 
+			parent_department_id, status, capacity_total_beds, capacity_available_beds, 
+			capacity_operating_rooms, operating_hours_weekday, operating_hours_weekend, 
+			operating_hours_timezone, operating_hours_holidays, department_head_id,
+			metadata, created_at, updated_at
+		) VALUES (
+			$1, $2, $3, $4, $5, $6, 
+			$7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $19
+		) RETURNING id, updated_at`
+)
+
+func (r *staffRepository) Create(ctx context.Context, staff *Staff) (*Staff, error) {
+	r.log.Debug("repository : Create : begin", nil)
+
+	r.log.Debug("repository : Create : exit", nil)
+	return nil, nil
+}
