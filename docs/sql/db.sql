3e408485db3b3ea5b28ae7efc5587aac77ecea7b
keerthana <keerthanaselvam05@gmail.com>
2025-03-17 20:10:10 +0530

    Add UserTypes dB Query

--- docs/sql/db.sql
+++ docs/sql/db.sql
@@ -28,7 +28,6 @@ CREATE TABLE staffs (
     UNIQUE(branch_id, code)
 );
 
-CREATE TYPE user_type AS ENUM ('regular', 'vip', 'corporate', 'senior citizen');
 CREATE TYPE user_status AS ENUM ('active', 'inactive', 'blocked');
  
 CREATE TABLE users (
@@ -39,7 +38,7 @@ CREATE TABLE users (
     last_name VARCHAR(100) NOT NULL,
     code VARCHAR(50) NOT NULL UNIQUE,
     status user_status NOT NULL,
-    type user_type NOT NULL,
+    user_type UUID NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR(20) NOT NULL,
     date_of_birth DATE NOT NULL,
@@ -55,7 +54,18 @@ CREATE TABLE users (
     UNIQUE(branch_id, code)
 );
 
+CREATE TABLE usertypes (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    metadata JSONB DEFAULT '{}'::JSONB,      
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
 CREATE INDEX staffs_branch_id_idx ON staffs(branch_id);
 CREATE INDEX staffs_organization_id_idx ON staffs(organization_id);
 CREATE INDEX users_branch_id_idx ON users(branch_id);
 CREATE INDEX users_organization_id_idx ON users(organization_id);
+CREATE INDEX usertypes_id_idx ON usertypes(id);
